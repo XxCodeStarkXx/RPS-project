4476c2af94b322bde9037349ff9cf8d30413c5e2
XxCodeStarkxX <semperfitrios@gmail.com>
2025-01-06 15:25:07 -0600

    added function playround added if else statments

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 let humanScore = 0;
 let computerScore = 0;
 
+
+
 function getComputerChoice() {
-    const strings = ["Rock", "Paper", "Scissors"]
-    const randomIndex = Math.floor(Math.random() * strings.length);
-    return strings[randomIndex];
+    const choices = ["rock", "paper", "scissors"]
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    const compChoice = choices[randomIndex]
+    return compChoice
+    
 }
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 
-//prompt("Rock, Paper, or Scissors?")
 
 function getHumanChoice() {
-     return prompt("Roshambo? Type: Rock, Paper, Scissors");
+    let choice = prompt("Roshambo? Type rock, paper, scissors")
+    choice = choice.toLowerCase();
+    if (choice === "rock" || choice === "paper" || choice === "scissors")
+        return choice;
+    else {
+        alert("Please enter a valid choice");
+        return getHumanChoice();
+    }
 }
 
-console.log(getHumanChoice());
+//console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+    console.log(humanChoice, computerChoice);
+    if (humanChoice === computerChoice) {
+        return "Its a tie";
+    } else if ((humanChoice === "rock" && computerChoice == "scissors") || (humanChoice === "paper" && computerChoice == "scissors") || (humanChoice === "paper" && computerChoice == "rock")) {
+        return "Player wins";
+    } else {
+        return "Computer wins";
+    }
+    
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+
